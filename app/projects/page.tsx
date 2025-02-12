40ed395ccefb577d3a08fadfcae7d625f5990dcc
Maxence Rault <raultmaxence05@gmail.com>
2025-02-12 17:15:34 +0100

    Finition Home page + début page projet

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { ParticleCanvas } from "@/hooks/particle";
-import { motion, useScroll, useTransform } from "framer-motion";
-import Navbar from "@/components/navbar";
+
 import Projects from "@/components/project";
 
 export default function Project() {
