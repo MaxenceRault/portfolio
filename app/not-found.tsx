1e106110fc6fe8a609c0654759e803b2639dddc5
MaxenceRault <raultmaxencee05@gmail.com>
2025-03-24 14:01:11 +0100

    Ajout page 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,10 +12,10 @@ export default function NotFound() {
       {/* Contenu centré */}
       <div className="z-10 p-10 bg-white/5 backdrop-blur-md rounded-2xl shadow-lg text-center">
         <h1 className="text-5xl md:text-6xl font-extrabold text-white drop-shadow-lg">
-          404 – Page introuvable
+          404 &ndash; Page introuvable
         </h1>
         <p className="mt-4 text-lg text-gray-300">
-          Oups... Cette page n'existe pas ou n'existe plus.
+          Oups... Cette page n&apos;existe pas ou n&apos;existe plus.
         </p>
         <Link href="/" passHref className="mt-6 inline-block px-6 py-3 rounded-xl text-white bg-blue-600 hover:bg-blue-700 transition-all duration-300 shadow-md">
           Revenir à l’accueil
